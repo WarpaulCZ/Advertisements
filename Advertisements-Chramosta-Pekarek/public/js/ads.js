66f60fe570aaef92d96e0158eb52cf18f200967c
Jakub Chramosta <jakub.chramosta1@gmail.com>
2021-04-08 14:03:59 +0200

    index ad renderer added

---
+++ Advertisements-Chramosta-Pekarek/public/js/ads.js
@@ -0,0 +1,14 @@
+window.onload = (ads) => {
+
+    const adsTimeLine = document.getElementById("AdTimeLine")
+
+    ads.forEach((ad) => {
+        const dadElement = document.createElement('div')
+        const adTopic = document.createElement('h2')
+        const adDescription = document.createElement('p')
+
+        adTopic.setText
+
+        filmElement.appendChild(filmEl)
+    });
+}
